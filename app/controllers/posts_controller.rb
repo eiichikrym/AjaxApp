d159e62dd97e5e3585c47c895596efa587fa85b0
eiichikrym <eiichikrym@gmail.com>
2020-07-22 17:01:41 +0900

    既読機能サーバーサイド完了
    エンドポイントはpathパラメーター
    データ形式はJson
    で設定しています。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
   def new
@@ -9,6 +9,22 @@ class PostsController < ApplicationController
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
-  
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked then
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
+
+# 確認すべき事
+# thenは必要？
+# render json: { post: item }の波括弧の意味がわからない。
