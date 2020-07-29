50ac1ed8ce386b33d20416e64694c47efbdf1369
eiichikrym <eiichikrym@gmail.com>
2020-07-29 16:07:48 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/posts", to:"posts#index"
-  # root to: "posts#index"
+  # get "/posts", to:"posts#index"
+  get "/", to:"posts#index"
+  root to: "posts#index"
   post 'posts', to: "posts#create"
   get '/posts/:id', to: "posts#checked"
 end
